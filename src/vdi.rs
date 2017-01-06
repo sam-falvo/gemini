9ee9ef99bd43ac1c9afec8e67a641bf1f96f4ddb
Samuel A. Falvo II <kc5tja@arrl.net>
2017-01-05 20:09:26 -0800

    draw_point and start of get_point

--- src/vdi.rs
+++ src/vdi.rs
@@ -18,9 +18,9 @@
 
 
 use sdl2;
-use sdl2::{render, video};
+use sdl2::{pixels, render, video};
 
-use std::result;
+use std::{result, vec};
 
 
 /// Indication of an error somewhere inside the VDI module.
@@ -32,11 +32,24 @@ pub enum VdiError {
 
 
 /// VDI drivers must conform to this interface.
+///
+/// A word about color indices.  Currently, only two indices are supported.
+/// Indices 0...127 corresponds to black, while indices 128...255 corresponds
+/// to white.  For future compatibility, use index 255 to refer to white.
 pub trait VDI {
+    /// Draw a single point at the provided coordinates.  Attempts to draw beyond
+    /// the edge of the surface will simply be ignored.
+    fn draw_point(&mut self, (u16, u16), u8);
+
+    /// Retrieves the current pixel value at a given position.
+    fn get_point(&self, (u16, u16)) -> u8;
 }
 
 
-/// This structure records the state of the VDI.
+/// This structure represents an SDL2-backed VDI surface (bluntly, a window).
+/// The window is fixed in size, emulating the frame buffer of a given size.
+/// When the window opens, the state of the frame buffer is completely undefined.
+/// You'll need to paint the frame buffer to establish a known image.
 pub struct SDL2Vdi<'a> {
     /// The dimensions field allows for a display surface up to 64Kx64K in size.
     dimensions: (u16, u16),
@@ -52,15 +65,27 @@ pub struct SDL2Vdi<'a> {
 
     /// Renderer (from which we can get the window again if we need to)
     renderer: render::Renderer<'static>,
+
+    /// Texture used to contain the frame buffer for the window.
+    texture: render::Texture,
+
+    /// Back-buffer to support get_point().
+    backbuffer: Vec<u8>,
 }
+
 
 impl<'a> SDL2Vdi<'a> {
     /// Create a new SDL2-backed VDI instance.
     /// This will open a window and
     /// create an appropriately-sized frame buffer to back it.
     /// At present, the bitmap is monochrome: 0s are black, 1s are white.
+    ///
+    /// width and height are measured in pixels.
     pub fn new(context: &'a sdl2::Sdl, width: u16, height: u16, title: &'a str) ->
                 result::Result<SDL2Vdi<'a>, VdiError> {
+        let total_pixels = width as usize * height as usize;
+        let backbuffer = Vec::with_capacity(total_pixels);
+
         let video_subsystem = match context.video() {
             Err(e) =>
                 return Err(VdiError::FromSdl(e)),
@@ -102,16 +127,71 @@ impl<'a> SDL2Vdi<'a> {
                r
         };
 
+        let mut t : render::Texture = match (&r).create_texture(
+                pixels::PixelFormatEnum::ARGB8888,
+                render::TextureAccess::Streaming,
+                width as u32, height as u32
+        ) {
+            Err(render::TextureValueError::WidthOverflows(_)) =>
+                return Err(VdiError::FromSdl(String::from("Width overflow"))),
+
+            Err(render::TextureValueError::HeightOverflows(_)) =>
+                return Err(VdiError::FromSdl(String::from("Height overflow"))),
+
+            Err(render::TextureValueError::WidthMustBeMultipleOfTwoForFormat(_, _)) =>
+                return Err(VdiError::FromSdl(String::from("Texture width must be a power of two."))),
+
+            Err(render::TextureValueError::SdlError(s)) =>
+                return Err(VdiError::FromSdl(s)),
+
+            Ok(t) =>
+                t,
+        };
+
+        (&mut t).set_blend_mode(render::BlendMode::None);
+
         Ok(SDL2Vdi {
             dimensions: (width, height),
             title:      title,
             sdl:        context,
             video:      video_subsystem,
             renderer:   r,
+            texture:    t,
+            backbuffer: backbuffer,
         })
     }
 }
 
+
 impl<'a> VDI for SDL2Vdi<'a> {
+    fn draw_point(&mut self, at: (u16, u16), pen: u8) {
+        let (x, y) = at;
+    let (width, height) = self.dimensions;
+
+        if (x >= width) || (y >= height) {
+            return;
+        }
+
+        let p = if pen >= 128 { 255 } else { 0 };
+
+        (&mut self.texture).with_lock(None, |bits: &mut [u8], span: usize| {
+            let (x, y) = (x as usize, y as usize);
+            let offset = y * span + (4 * x);
+            bits[offset+0] = p;
+            bits[offset+1] = p;
+            bits[offset+2] = p;
+            bits[offset+3] = p;
+        });
+    }
+
+    fn get_point(&self, at: (u16, u16)) -> u8 {
+        0
+    }
+}
+
+impl<'a> Drop for SDL2Vdi<'a> {
+    fn drop(&mut self) {
+        println!("DROPPING SDL2Vdi");
+    }
 }
 
--- tests/vdi.rs
+++ tests/vdi.rs
@@ -3,13 +3,29 @@
 extern crate gemini;
 extern crate sdl2;
 
-use std::{thread, time};
 use gemini::vdi;
 
+
 #[test]
+#[allow(unused_variables)]
 fn creation() {
     let sdl_context = sdl2::init().unwrap();
     let vdi : &mut vdi::VDI = &mut vdi::SDL2Vdi::new(&sdl_context, 640, 480, "Fake VGA").unwrap();
-    let vdi2 : &mut vdi::VDI = &mut vdi::SDL2Vdi::new(&sdl_context, 512, 384, "Fake Mac").unwrap();
-    thread::sleep(time::Duration::new(20,0));
 }
+
+fn draw_point() {
+    let sdl = sdl2::init().unwrap();
+    let vdi : &mut vdi::VDI =
+        &mut vdi::SDL2Vdi::new(&sdl, 640, 480, "blah").unwrap();
+
+    for i in 0..128 {
+        vdi.draw_point((0, 0), i);
+        assert_eq!(vdi.get_point((0, 0)), 0);
+    }
+
+    for i in 128..256 {
+        vdi.draw_point((0, 0), i as u8);
+        assert_eq!(vdi.get_point((0, 0)), 255);
+    }
+}
+
